5ebaea4daf64fc03b1c0790d86abbb82aab274cb
holdless <holdless2@gmail.com>
2017-02-01 17:33:27 +0800

    win_air

--- Users/Hiroshi/Documents/bat/gitwin.bat
+++ Users/Hiroshi/Documents/bat/gitwin.bat
@@ -1,6 +1,6 @@
 git add C:/Users/Hiroshi/Documents/bat/*
 
-git add C:/Users/Hiroshi/Documents/Visual Studio 2013/Projects/loadCaffe/
+git add C:/Users/Hiroshi/Documents/Visual Studio 2013/Projects/loadCaffe/*
 
 git commit -m "win_air"
 git push -u origin master
